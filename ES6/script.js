96bdcb1f26f2269367dc614ac62e47f46ffac831
felixandrewsapalaran <felixandrewsapalaran@gmail.com>
2021-05-28 19:22:10 -0400

    Template Literals for easier concatenation of strings

--- ES6/script.js
+++ ES6/script.js
@@ -1,37 +1,16 @@
 /*
-Let & Const
+    Template Literals
 */
 
+let firstName = "Andrew";
+let lastName = "Sapalaran";
 
-// Let example
-function letVarExample(){
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
-    if (true) {
-        
-        var firstName = "Andrew"; // exists in function scope, defined in function scope
-        let lastName = "Sapalaran"; // exists in block scope only, defined in block scope
+let searchResult = 50;
 
-        // You can only access lastName value inside this block
-        console.log("Function Scope Access: ",lastName);
-    }
-
-    console.log("Function Scope Access: ", firstName);
-    //console.log("Function Scope Access: ",lastName); // Error: wont be able to access lastName value in this scope
-}
-
-letVarExample()
-
-
-// Const example 1
-// initiliazing const 
-const name = "Andrew";
-// try changing constant value
-name = "Andrew E";
-console.log(name); // Error: cant change value of a constant variable
-
-
-// Const example 2
-const array = [1, 2, 3];
-array.push(4); //adding 4 at the end of the array
-console.log(array); // this works because the address in memory didn't change it just expand
-
+// if searchResult is greater than 0 then display `{searchResult value} result`
+// otherwise display `No search result`
+let output = `${searchResult > 0 ? `${searchResult} result`: "No search result"}`
+console.log(output);
