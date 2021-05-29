65be57ea6f2247de6aff21e5dca1385b092b6a76
felixandrewsapalaran <felixandrewsapalaran@gmail.com>
2021-05-29 16:36:15 -0400

    Setting Default Paramaters in functions

--- ES6/script.js
+++ ES6/script.js
@@ -19,7 +19,7 @@ console.log(output);
 
 /*
     Arrow Functions
-*/
+
 
 // // Simple function
 // function getFullName() {
@@ -50,3 +50,17 @@ console.log(`${firstName} ${lastName}`);
 // call the function and pass the required parameter
 getFullNameShorter(firstName, lastName);
 
+*/
+
+
+/*
+ Default Parameters
+*/
+
+// if sortType has no value pass in use default 'Name'
+sortBy = (sortType = "Name", users) => {
+    console.log("Sorting By: ", sortType);
+}
+
+sortBy(); //Passed in no value so use default `Name`
+sortBy("Date", []); //Passed in `Date` as value
