c0b9c043f53715748aaa997977fa8cb58c503ee0
felixandrewsapalaran <felixandrewsapalaran@gmail.com>
2021-05-29 16:21:44 -0400

    Using Arrow Functions to define functions easier

--- ES6/script.js
+++ ES6/script.js
@@ -1,6 +1,6 @@
 /*
     Template Literals
-*/
+
 
 let firstName = "Andrew";
 let lastName = "Sapalaran";
@@ -14,3 +14,39 @@ let searchResult = 50;
 // otherwise display `No search result`
 let output = `${searchResult > 0 ? `${searchResult} result`: "No search result"}`
 console.log(output);
+
+*/
+
+/*
+    Arrow Functions
+*/
+
+// // Simple function
+// function getFullName() {
+//     let firstName = "Felix";
+//     let lastName = "Sapalaran";
+//     return `${firstName} ${lastName}`;
+// }
+
+
+// Arrow function
+getFullName = () => {
+    let firstName = "Andrew";
+    let lastName = "Sapalaran";
+    return `${firstName} ${lastName}`;
+}
+
+console.log(getFullName());
+
+
+
+let firstName = "G-boy";
+let lastName = "Labitoria";
+
+// Arrow function shorter version
+getFullNameShorter = (firstName, lastName) =>
+console.log(`${firstName} ${lastName}`);
+
+// call the function and pass the required parameter
+getFullNameShorter(firstName, lastName);
+
